dc5752a9c5c17e128beea0bad3d94744677bbe88
Uday <udaysagar.mail@gmail.com>
2025-04-22 23:06:02 +0530

    cache results

--- src/app/api/getResults/route.ts
+++ src/app/api/getResults/route.ts
@@ -1,13 +1,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import getResultsById from "@/actions/getResultsById";
+import redis from "@/utils/redis";
 
 export async function POST(request: NextRequest) {
   const { students, examId } = await request.json();
 
   const results = await Promise.all(
     students.map(async (student: any) => {
+      const redisKey = `result:${student.rollNumber}:${examId}`;
+      const cachedResult = await redis.get(redisKey);
+
+      if (cachedResult) {
+        const { id, ...studentData } = student;
+        return {
+          student: studentData,
+          result: cachedResult,
+        };
+      }
+
       const result = await getResultsById(student.rollNumber, examId);
       const { id, ...studentData } = student;
+
+      await redis.set(redisKey, result, { ex: 15552000 }); // 180 days
+
       return {
         student: studentData,
         result,
